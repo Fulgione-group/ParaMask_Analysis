59141cd3914c38235cad43b59d7618e6f2946905
Bastiaan Tjeng <btjeng@nb-s7-022.mpipz.mpg.de>
2024-03-15 17:08:24 +0100

    Addd text Diversity_estimates

--- Diversity_estimates/Scripts/SpanishArabisAlpina/calculate_div_ES04_collapsed.sh
+++ Diversity_estimates/Scripts/SpanishArabisAlpina/calculate_div_ES04_collapsed.sh
@@ -3,7 +3,7 @@
 java -jar /home/btjeng/Data/SeDuS/local_SeDuS_runs/ParaMask_Calculate_Diversity.jar \
         --vcf GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES04Paramask.10PerMissing.snps.b.vcf.gz\
         --cutoff 0.1\
-        --seqlength 171043752\
+        --seqlength 171043752\ #accounting for density with missing data of all site VCF
         --id ES04_collapsed\
         --replicate 1
 
