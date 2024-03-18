3dfc6b218250f8116981e096c81063e4892f50fe
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-18 14:04:29 +0100

    Create Flye_assembly.sh

---
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/Flye_assembly.sh
@@ -0,0 +1,11 @@
+table_file="/netscratch/dep_coupland/grp_fulgione/male/assemblies/assemblies_for_script.txt"
+while IFS=$'\t' read -r col1 col2
+do
+
+ID="${col1}"
+raw_data="${col2}"
+genome_size="ESTIMATED_GENOME_SIZE"
+
+cd /netscratch/dep_coupland/grp_fulgione/male/assemblies/${ID}/flye/
+bsub -q bigmem -R "rusage[mem=200000]" -M 250000 "flye --pacbio-hifi ${raw_data} -o /netscratch/dep_coupland/grp_fulgione/male/assemblies/${ID}/flye -g ${genome_size} -t 45"
+done < "$table_file"
