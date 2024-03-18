a84b48985309a58e0bd479992a4717a01f8a7dd4
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-18 15:16:16 +0100

    Update Calculate_coverage_per_base.sh

--- ParaMask_validation_with_longreads/Coverage_analysis_longread_assemblies/Calculate_coverage_per_base.sh
+++ ParaMask_validation_with_longreads/Coverage_analysis_longread_assemblies/Calculate_coverage_per_base.sh
@@ -2,5 +2,20 @@
 
 BAM_file ="/Path/to/Bam_file.bam"
 ID="Sample_ID"
+WINDOW_SIZE=1500
+OUTPUT_BED="$SCRIPT_DIR/mean_coverage.bed"
 
-samtools depth -a ${BAM_file} > ${ID}_coverage.coverage
+samtools depth -a ${ID}_pb.paf.gz | \
+    awk -v window="$WINDOW_SIZE" 'BEGIN{OFS="\t"; start=1; end=window} {
+        while (start < $2) {
+            total=0; count=0
+            for (i=start; i<=end; i++) {
+                total+=$3; count++
+            }
+            if (count > 0) {
+                print $1, start, end, total/count
+            }
+            start+=window; end+=window
+        }
+    }' > "$OUTPUT_BED"
+
