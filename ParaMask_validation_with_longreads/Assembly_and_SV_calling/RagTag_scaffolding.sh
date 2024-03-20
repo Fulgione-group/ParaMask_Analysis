a77558ce1cbb0372f43b9d911a09ba1ef5dc5ad2
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 09:30:54 +0100

    Update RagTag_scaffolding.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/RagTag_scaffolding.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/RagTag_scaffolding.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-reference="/netscratch/dep_coupland/grp_fulgione/male/assemblies/Reference_v5.1_analysis/Reference_v5.1_updated.fasta"
-assembly="ASSEMBLY"
-ID=""
-mapper="PATH_TO_MINIMAP_OR_NUCMER"
+reference="/Path/to/reference.fasta"
+assembly="/Path/to/assembly.fasta"
+ID="Accession_ID"
+Ragtag="/Path/to/ragtag/"
+mapper="/Path/to/mapper/minimap"
 
-export PATH="/home/marimond/.conda/envs/bases/bin/:$PATH"
+export PATH="${Ragtag}/bin/:$PATH"
+
 # without chimeric contig correction
-bsub -q multicore20 -R "rusage[mem=35000]" -M 45000 "ragtag.py scaffold ${reference} ${assembly} -o ${ID}_RagTag -C --aligner ${mapper} -t 20"
+ragtag.py scaffold ${reference} ${assembly} -o ${ID}_RagTag -C --aligner ${mapper} -t 20
 
-export PATH="/home/marimond/.conda/envs/bases/bin/:$PATH"
-bsub -q multicore20 -R "rusage[mem=35000]" -M 45000 "ragtag.py scaffold /netscratch/dep_coupland/grp_fulgione/male/assemblies/Reference_v5.1_analysis/Reference_v5.1_updated.fasta /netscratch/dep_coupland/grp_fulgione/male/assemblies/SI01_005/merged_flye_hifiasm/merged_out.fasta -C -t 20"
