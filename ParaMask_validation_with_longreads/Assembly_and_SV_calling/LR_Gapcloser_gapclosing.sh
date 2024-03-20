c933b4a1efda66cac6b661e34bbe66faa153c214
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 09:08:37 +0100

    Update LR_Gapcloser_gapclosing.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/LR_Gapcloser_gapclosing.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/LR_Gapcloser_gapclosing.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-export PATH="/netscratch/dep_coupland/grp_fulgione/male/assemblies/LR_Gapcloser/src/:$PATH"
-bsub -q multicore40 -R "rusage[mem=75000]" -M 80000 "bash LR_Gapcloser.sh -i /netscratch/dep_coupland/grp_fulgione/male/assemblies/SE02_002/RagTag_nucmer/ragtag_wo_corr/ragtag.scaffold.fasta -l SE02_002_raw.fasta -o LR_Gapcloser/"
+path_to_LR_Gapcloser="path/to/LR_Gapcloser/"
+raw_data="/Path/to/raw_data.fastq"
+assembly="/Path/to/Scaffolds.fasta"
+working_dir="/Path/to/working_dir/"
+output_dir="/Path/to/output_dir/"
 
-input_file="/netscratch/dep_coupland/grp_fulgione/male/assemblies/SC_assemblies.txt"
-lsfenv
-while read -r col1 col2 _; do
-  samtools faidx /netscratch/dep_coupland/grp_fulgione/male/assemblies/${col1}/RagTag_nucmer/ragtag_output/ragtag.scaffold.fasta
-  echo "Index of ${col1} created"
-  seqtk seq -a ${col2} > /netscratch/dep_coupland/grp_fulgione/male/assemblies/${col1}/${col1}_raw.fasta
-  export PATH="/netscratch/dep_coupland/grp_fulgione/male/assemblies/LR_Gapcloser/src/:$PATH"
-  bsub -q multicore40 -R "rusage[mem=75000]" -M 80000 "bash LR_Gapcloser.sh -i /netscratch/dep_coupland/grp_fulgione/male/assemblies/${col1}/RagTag_nucmer/ragtag_output/ragtag.scaffold.fasta -l /netscratch/dep_coupland/grp_fulgione/male/assemblies/${col1}/${col1}_raw.fasta -m 1000 -o /netscratch/dep_coupland/grp_fulgione/male/assemblies/${col1}/LR_Gapcloser"
-done < "$input_file"
+cd ${working_dir}
+
+#Index Scaffolds
+samtools faidx ${assembly}
+
+#Run LR Gapcloser
+export PATH="${path_to_LR_Gapcloser}/src/:$PATH"
+bash LR_Gapcloser.sh -i ${assembly} -l ${raw_data} -o ${output_dir}
+
+
