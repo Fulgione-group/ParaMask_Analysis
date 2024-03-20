437346fbc081c5ae03e9e71e3005df6148c0614d
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 08:59:29 +0100

    Update Flye_assembly.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/Flye_assembly.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/Flye_assembly.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-table_file="/netscratch/dep_coupland/grp_fulgione/male/assemblies/assemblies_for_script.txt"
-while IFS=$'\t' read -r col1 col2
-do
+table_file="/Path/to/table_file.txt"  #including accession ID (col1) and raw read paths (col 2)
+  while IFS=$'\t' read -r col1 col2
+    do
 
-ID="${col1}"
-raw_data="${col2}"
-genome_size="ESTIMATED_GENOME_SIZE"
+    ID="${col1}"
+    raw_data="${col2}"
+    genome_size="ESTIMATED_GENOME_SIZE"
+    working_dir="/Path/tp/work_dir/"
+    output_dir="/Path/to/output_dir/"
 
-cd /netscratch/dep_coupland/grp_fulgione/male/assemblies/${ID}/flye/
-bsub -q bigmem -R "rusage[mem=200000]" -M 250000 "flye --pacbio-hifi ${raw_data} -o /netscratch/dep_coupland/grp_fulgione/male/assemblies/${ID}/flye -g ${genome_size} -t 45"
+    cd ${working_dir}
+    bsub -q bigmem -R "rusage[mem=200000]" -M 250000 "flye --pacbio-hifi ${raw_data} -o ${output_dir} -g ${genome_size} -t 45"
 done < "$table_file"
