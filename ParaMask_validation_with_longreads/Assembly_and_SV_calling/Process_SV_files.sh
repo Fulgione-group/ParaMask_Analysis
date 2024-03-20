d8bad96a9e7137f92e17fe46f867a1163c5791e8
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 09:12:58 +0100

    Update Process_SV_files.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/Process_SV_files.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/Process_SV_files.sh
@@ -3,5 +3,5 @@ VCF_file="/path/VCF_file.vcf"
 ID="Sample_ID"
 
 # Grep only duplications 
-bcftools query -f '%CHROM\t%POS\t%INFO/END\t%INFO/SVLEN\t%INFO/SVTYPE\n' /biodata/dep_coupland/grp_fulgione/male/sv_calling/${ID}/survivor/${VCF_file} | grep 'DUP' | grep 'chr' > ${ID}_dup.txt
+bcftools query -f '%CHROM\t%POS\t%INFO/END\t%INFO/SVLEN\t%INFO/SVTYPE\n' ${VCF_file} | grep 'DUP' | grep 'chr' > ${ID}_dup.txt
 echo -e "Chromosome\tSTART\tSTOP\tLENGTH\tTYPE" > ${ID}_dup_with_header.txt && cat ${ID}_dup.txt >> ${ID}_dup_with_header.txt
