9610f75217b690be141c1d8207ffb1d6739c38d9
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 09:12:24 +0100

    Update Merge_SV_files_survivor.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/Merge_SV_files_survivor.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/Merge_SV_files_survivor.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
-# SURVIVOR (merge both sets)
-ID="ID"
-/home/marimond/.conda/envs/bases/bin/SURVIVOR merge ${ID}_vcf_files.txt 0.1 2 1 0 1 35 ${ID}_only_overlapping_SVs.vcf
-/home/marimond/.conda/envs/bases/bin/SURVIVOR merge ${ID}_vcf_files.txt 0.1 1 1 0 1 35 ${ID}_all_SVs.vcf
+#Merge SV sets of sniffles and cuteSV
+
+Survivor="/Path/to/survivor/"
+ID="Accession_ID"
+
+#Merge only overlapping SVs with allowed overlapping flanking region: 10% of SV size
+${Survivor}/bin/SURVIVOR merge ${ID}_vcf_files.txt 0.1 2 1 0 1 35 ${ID}_only_overlapping_SVs.vcf
+
+
+#Merge all SVs
+${Survivor}/bin/SURVIVOR merge ${ID}_vcf_files.txt 0.1 1 1 0 1 35 ${ID}_all_SVs.vcf
