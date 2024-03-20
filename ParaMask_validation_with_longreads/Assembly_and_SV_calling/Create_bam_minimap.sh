00f94ceac11f7a2d94f0493a5f8c56b8c2556637
malearimond <118990401+malearimond@users.noreply.github.com>
2024-03-20 08:55:08 +0100

    Update Create_bam_minimap.sh

--- ParaMask_validation_with_longreads/Assembly_and_SV_calling/Create_bam_minimap.sh
+++ ParaMask_validation_with_longreads/Assembly_and_SV_calling/Create_bam_minimap.sh
@@ -1,15 +1,9 @@
 #!/bin/bash
 
-work_dir="/path/to/your/workingdirectory"
-
 ID="accession_ID"
-
 raw_reads="/path/to/your/raw_reads.fastq"
+reference="/path/to/your/reference.fa"        
 
-reference="/path/to/your/reference.fa"        #/netscratch/dep_coupland/grp_fulgione/male/assemblies/Reference_v5.1_analysis/Reference_v5.1_updated.fasta
-
-# set working directory
-cd ${work_dir}
 
 
 # Map raw reads against reference
