d20662bfed5f84caaafdc1a01fa36b6c4734f31c
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:21:07 +0100

    Update vcftools_ParaMask_Fst.sh

--- Fst_Fis_estimates/SpanishArabisAlpina/vcftools_ParaMask_Fst.sh
+++ Fst_Fis_estimates/SpanishArabisAlpina/vcftools_ParaMask_Fst.sh
@@ -2,10 +2,10 @@
 source /netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/anaconda3/etc/profile.d/conda.sh
 conda activate baconda
 
-vcftools --gzvcf /netscratch/dep_coupland/grp_fulgione/bastiaan/data/GATKfinal/GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.gz\
- --out /netscratch/dep_coupland/grp_fulgione/bastiaan/data/GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.FST\
- --weir-fst-pop /netscratch/dep_coupland/grp_fulgione/bastiaan/data/Pop_doc/ES03_1000Genomes_IDonly.txt\
- --weir-fst-pop /netscratch/dep_coupland/grp_fulgione/bastiaan/data/Pop_doc/ES04_1000Genomes_IDonly.txt\
+vcftools --gzvcf GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.gz\
+ --out GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.FST\
+ --weir-fst-pop ES03_1000Genomes_IDonly.txt\
+ --weir-fst-pop ES04_1000Genomes_IDonly.txt\
  --fst-window-size 400000000\  ##big window to cover the genome
  --fst-window-step 400000000
 wait
