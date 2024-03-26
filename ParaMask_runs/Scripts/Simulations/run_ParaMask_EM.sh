224306e7b1a3fde03f88c347748b88f98a4fbe80
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:24:47 +0100

    Update run_ParaMask_EM.sh

--- ParaMask_runs/Scripts/Simulations/run_ParaMask_EM.sh
+++ ParaMask_runs/Scripts/Simulations/run_ParaMask_EM.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-source /netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/anaconda3/etc/profile.d/conda.sh
-conda activate Paramask_env
+source PATH_TO_CONDA/conda.sh
+conda activate Paramask_env #for conda environment with all prerequisites
 
 
 ##$1= hetfile, $2=output dir, $3=ID
 
 ulimit -v 50000000
 export OPENBLAS_NUM_THREADS=40 OMP_NUM_THREADS=40 MKL_NUM_THREADS=40
-Rscript --vanilla ~/scripts/ParaMask_simulation_scripts/ParaMask_EM_v2.4.R "--hetfile" $1 \
+Rscript --vanilla PATH_TO_PARAMASK/ParaMask_EM_v2.4.R "--hetfile" $1 \
 "--outpath" $2 "--missingness" "0.1" "--verbose" "--ID" $3
