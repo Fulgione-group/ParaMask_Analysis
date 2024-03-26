730a8ac158883bf52a64731fa8f38e6b6348d38a
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:11:20 +0100

    Update markduplicated_and_merge_all2.sh

--- SNP_calling/Scripts/markduplicated_and_merge_all2.sh
+++ SNP_calling/Scripts/markduplicated_and_merge_all2.sh
@@ -7,7 +7,7 @@ structure_file=$2
 maxjob=$(expr $3 - 1 )
 
 
-gatk2='/netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/gatk-4.2.0.0/gatk'
+gatk2='PATH_TO_GATK/gatk-4.2.0.0/gatk'
 
 
 
