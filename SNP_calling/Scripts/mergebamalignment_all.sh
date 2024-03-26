5f39224b73397315ef972e82c0813d0ea8628a7f
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:11:51 +0100

    Update mergebamalignment_all.sh

--- SNP_calling/Scripts/mergebamalignment_all.sh
+++ SNP_calling/Scripts/mergebamalignment_all.sh
@@ -5,7 +5,7 @@ samples=$2
 maxjob=$3
 cd $dir
 
-gatk2='/netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/gatk-4.2.0.0/gatk'
+gatk2='PATH_TO_GATK/gatk-4.2.0.0/gatk'
 
 
 
@@ -15,7 +15,7 @@ mergebamalignment () {
         uj=$(echo $j | rev | cut -d '.' -f2- | rev)
 	sample=$(echo $uj | rev | cut -d '_' -f2- | rev)
 	$gatk2 --java-options "-XX:ConcGCThreads=1 -XX:ParallelGCThreads=1 -XX:ActiveProcessorCount=1" MergeBamAlignment \
-        	-R /netscratch/dep_coupland/grp_fulgione/bastiaan/data/reference/Alpina_V5.1/Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
+        	-R Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
         	--UNMAPPED_BAM ${uj} \
         	--ALIGNED_BAM ${sample}_samtofastq_bwamem.sam \
         	-O ${sample}_mergebamalignment.bam \
