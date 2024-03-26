e39dc82c9a650116595da72247e4bc1a5f851f65
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:08:06 +0100

    Update gDBimport_Alpina_Genomes.sh

--- SNP_calling/Scripts/gDBimport_Alpina_Genomes.sh
+++ SNP_calling/Scripts/gDBimport_Alpina_Genomes.sh
@@ -3,10 +3,10 @@
 export TILEDB_DISABLE_FILE_LOCKING=1
 
 cd /netscratch/dep_coupland/grp_fulgione/bastiaan/data/GATK_DBs/
-gatk2='/netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/gatk-4.2.0.0/gatk'
+gatk2='PATH_TO_GATK/gatk-4.2.0.0/gatk'
 
 $gatk2 --java-options "-XX:ConcGCThreads=1 -XX:ActiveProcessorCount=3 -Xmx150G" GenomicsDBImport \
-	--sample-name-map /netscratch/dep_coupland/grp_fulgione/bastiaan/data/Pop_doc/gDB_sample_map_1000Genomes_sorted.txt \
+	--sample-name-map gDB_sample_map_1000Genomes_sorted.txt \
 	--max-num-intervals-to-import-in-parallel 1 \
 	-L chr1 \
 	-L chr2 \
@@ -16,7 +16,7 @@ $gatk2 --java-options "-XX:ConcGCThreads=1 -XX:ActiveProcessorCount=3 -Xmx150G" 
 	-L chr6 \
 	-L chr7 \
 	-L chr8 \
-	--genomicsdb-workspace-path /netscratch/dep_coupland/grp_fulgione/bastiaan/data/GATK_DBs/AlpinaGenomes \
-	--reference /netscratch/dep_coupland/grp_fulgione/bastiaan/data/reference/Alpina_V5.1/Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta
+	--genomicsdb-workspace-path PATH_TO_GATK_DBs/AlpinaGenomes \
+	--reference Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta
 
 
