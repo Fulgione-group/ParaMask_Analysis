0a8eb5a2f8d54c0ad02a70fec0250bc338e2151e
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:09:28 +0100

    Update genotypeGVCF_Alpina_Genomes.sh

--- SNP_calling/Scripts/genotypeGVCF_Alpina_Genomes.sh
+++ SNP_calling/Scripts/genotypeGVCF_Alpina_Genomes.sh
@@ -4,14 +4,14 @@ dir=$1
 echo "dir"
 cd $dir
 samples=$2
-gatk2='/netscratch/dep_coupland/grp_fulgione/bastiaan/software/gatk-4.2.1.0/gatk'
+gatk2='PATH_TO_GATK/gatk-4.2.1.0/gatk'
 
 
 $gatk2 --java-options "-DGATK_STACKTRACE_ON_USER_EXCEPTION=true -XX:ConcGCThreads=2 -XX:ActiveProcessorCount=4 -Xms10G -Xmx80G -DGATK_STACKTRACE_ON_USER_EXCEPTION=true" GenotypeGVCFs \
-    -R /netscratch/dep_coupland/grp_fulgione/bastiaan/data/reference/Alpina_V5.1/Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
-    -V gendb:///netscratch/dep_coupland/grp_fulgione/bastiaan/data/GATK_DBs/AlpinaGenomes \
+    -R Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
+    -V gendb://PATH_TO_GATK_DBs/AlpinaGenomes \
     --use-new-qual-calculator \
-    -O GATK4.2_alpina_15_02_2023_chr1.vcf \
+    -O GATK4.2_Alpina_Genomes.vcf \
     -all-sites\
     --tmp-dir /scratch\
     -L chr1
