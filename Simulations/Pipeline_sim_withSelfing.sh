47af81c31a6db301631978aebdc9da6f8a91283b
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:39:32 +0100

    Update Pipeline_sim_withSelfing.sh

--- Simulations/Pipeline_sim_withSelfing.sh
+++ Simulations/Pipeline_sim_withSelfing.sh
@@ -1,29 +1,24 @@
   
-
-
-###run sim before pipeline TBE
-
-
 ###transform all files 
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SC.sh PATH_TO_OUT_DIR
+bash transform_mutation_SC.sh PATH_TO_OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SV.sh PATH_TO_OUT_DIR
+bash transform_mutation_SV.sh PATH_TO_OUT_DIR
 
 ####pipeline for single run
 ##convert to vcf
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SC_withSelfing.sh 1 PATH_TO_OUT_DIR
+bash run_genotypetype_to_mat_SC_withSelfing.sh 1 PATH_TO_OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SV_new_withSelfing.sh 1 PATH_TO_OUT_DIR
+bash run_genotypetype_to_mat_SV_new_withSelfing.sh 1 PATH_TO_OUT_DIR
 #concat SV duplicates
-bash ~/scripts/ParaMask_simulation_scripts/concat_SV_mat_new.sh 1 PATH_TO_OUT_DIR
+bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUT_DIR
 #concat SV with SC
-bash ~/scripts/ParaMask_simulation_scripts/concat_SC_SV_mat.sh 1 PATH_TO_OUT_DIR
+bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUT_DIR
 #annotate SVtab
-bash ~/scripts/ParaMask_simulation_scripts/Annotate_SVtab.sh SVtab_0.5_rep1.txt
+bash Annotate_SVtab.sh SVtab.txt
 #update postion with
-bash ~/scripts/ParaMask_simulation_scripts/update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash update_pos_sim_mat.sh SVtab_annotated.txt Simulations_SV_SC_rep{REP_NUMBER}_mat.txt PATH_TO_OUT_DIR 
 #simulate allelic ratios and coverage
-bash ~/scripts/ParaMask_simulation_scripts/simulate_coverage_allele_depth.sh Simulations_SV_SC_rep1_mat_cpos.txt
+bash ~/scripts/ParaMask_simulation_scripts/simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
 
