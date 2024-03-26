4b3fc1300b8e4143fb3cc3c2f2cb986996144a67
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:33:09 +0100

    Update Pipeline_sim_withSelfing.sh

--- Simulations/Pipeline_sim_withSelfing.sh
+++ Simulations/Pipeline_sim_withSelfing.sh
@@ -6,22 +6,22 @@
 
 ###transform all files 
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SC.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SC.sh PATH_TO_OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SV.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SV.sh PATH_TO_OUT_DIR
 
 ####pipeline for single run
 ##convert to vcf
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SC_withSelfing.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SC_withSelfing.sh 1 PATH_TO_OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SV_new_withSelfing.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SV_new_withSelfing.sh 1 PATH_TO_OUT_DIR
 #concat SV duplicates
-bash ~/scripts/ParaMask_simulation_scripts/concat_SV_mat_new.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/concat_SV_mat_new.sh 1 PATH_TO_OUT_DIR
 #concat SV with SC
-bash ~/scripts/ParaMask_simulation_scripts/concat_SC_SV_mat.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
+bash ~/scripts/ParaMask_simulation_scripts/concat_SC_SV_mat.sh 1 PATH_TO_OUT_DIR
 #annotate SVtab
-bash ~/scripts/ParaMask_simulation_scripts/Annotate_SVtab.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final/SVtab_0.5_rep1.txt
+bash ~/scripts/ParaMask_simulation_scripts/Annotate_SVtab.sh SVtab_0.5_rep1.txt
 #update postion with
 bash ~/scripts/ParaMask_simulation_scripts/update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_withSelfing2
 #simulate allelic ratios and coverage
