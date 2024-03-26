9d67c7d3b2da686b2adb4d512966691e610bf540
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:36:54 +0100

    Update Pipeline_sim.sh

--- Simulations/Pipeline_sim.sh
+++ Simulations/Pipeline_sim.sh
@@ -18,9 +18,9 @@ bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR
 #concat SV with SC
 bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUTPUT_DIR
 #annotate SVtab
-bash Annotate_SVtab.sh  PATH_TO_OUTPUT_DIR
+bash Annotate_SVtab.sh  SVtab.txt
 #update postion with
-bash update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt  PATH_TO_OUTPUT_DIR
+bash update_pos_sim_mat.sh SVtab_annotated.txt Simulations_SV_SC_rep1_mat.txt  PATH_TO_OUTPUT_DIR
 #simulate allelic ratios and coverage
 bash simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
 
