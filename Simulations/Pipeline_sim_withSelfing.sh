1f1bae1a0e4f1bca300fa20af48c39a4f0fed217
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:42:05 +0100

    Update Pipeline_sim_withSelfing.sh

--- Simulations/Pipeline_sim_withSelfing.sh
+++ Simulations/Pipeline_sim_withSelfing.sh
@@ -1,24 +1,24 @@
   
 ###transform all files 
 #SC
-bash transform_mutation_SC.sh PATH_TO_OUT_DIR
+bash transform_mutation_SC.sh PATH_TO_SEDUS_OUTPUT_DIR
 #SV
-bash transform_mutation_SV.sh PATH_TO_OUT_DIR
+bash transform_mutation_SV.sh PATH_TO_SEDUS_OUTPUT_DIR
 
 ####pipeline for single run
 ##convert to vcf
 #SC
-bash run_genotypetype_to_mat_SC_withSelfing.sh REP_NUMBER PATH_TO_OUT_DIR
+bash run_genotypetype_to_mat_SC_withSelfing.sh REP_NUMBER PATH_TO_SEDUS_OUTPUT_DIR
 #SV
-bash run_genotypetype_to_mat_SV_new_withSelfing.sh REP_NUMBER PATH_TO_OUT_DIR
+bash run_genotypetype_to_mat_SV_new_withSelfing.sh REP_NUMBER PATH_TO_SEDUS_OUTPUT_DIR
 #concat SV duplicates
-bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUT_DIR
+bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_SEDUS_OUTPUT_DIR
 #concat SV with SC
-bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUT_DIR
+bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_SEDUS_OUTPUT_DIR
 #annotate SVtab
 bash Annotate_SVtab.sh SVtab.txt
 #update postion with
-bash update_pos_sim_mat.sh SVtab_annotated.txt Simulations_SV_SC_rep{REP_NUMBER}_mat.txt PATH_TO_OUT_DIR 
+bash update_pos_sim_mat.sh SVtab_annotated.txt Simulations_SV_SC_rep{REP_NUMBER}_mat.txt PATH_TO_SEDUS_OUTPUT_DIR 
 #simulate allelic ratios and coverage
-bash ~/scripts/ParaMask_simulation_scripts/simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
+bash simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
 
