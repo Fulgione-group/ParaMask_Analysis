cf65e680083e12ed8ab31a7bcff46c74bd39af8d
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:22:24 +0100

    Update vcftools_ParaMask_Fst.sh

--- Fst_Fis_estimates/SpanishArabisAlpina/vcftools_ParaMask_Fst.sh
+++ Fst_Fis_estimates/SpanishArabisAlpina/vcftools_ParaMask_Fst.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-source /netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/anaconda3/etc/profile.d/conda.sh
-conda activate baconda
 
 vcftools --gzvcf GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.gz\
  --out GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04Paramask.10PerMissing.snps.0.1.b.vcf.FST\
@@ -9,4 +7,3 @@ vcftools --gzvcf GATK4.2_1000Genomes_chrall.filteredQ30LD5UD100K.final.ES03ES04P
  --fst-window-size 400000000\  ##big window to cover the genome
  --fst-window-step 400000000
 wait
-conda deactivate
