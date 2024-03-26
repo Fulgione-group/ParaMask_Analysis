e7fb2729125341bdcfb1d5bfd116f93b8f99a8eb
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:34:20 +0100

    Update Pipeline_sim.sh

--- Simulations/Pipeline_sim.sh
+++ Simulations/Pipeline_sim.sh
@@ -3,23 +3,24 @@
 
 ###transform all files 
 #SC
-bash transform_mutation_SC.sh PATH_TO_OUTPUT_DIR/OUT_DIR
+bash transform_mutation_SC.sh PATH_TO_OUTPUT_DIR
 #SV
-bash transform_mutation_SV.sh  PATH_TO_OUTPUT_DIR/OUT_DIR
+bash transform_mutation_SV.sh  PATH_TO_OUTPUT_DIR
 
 ####pipeline for single run
 ##convert to vcf
 #SC
-bash run_genotypetype_to_mat_SC.sh REP_NUMBER  PATH_TO_OUTPUT_DIR/OUT_DIR
+bash run_genotypetype_to_mat_SC.sh REP_NUMBER  PATH_TO_OUTPUT_DIR
 #SV
-bash run_genotypetype_to_mat_SV_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIRconcat SV duplicates
-bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIR
+bash run_genotypetype_to_mat_SV_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR
+#concat SV duplicates
+bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR
 #concat SV with SC
-bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIR
+bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUTPUT_DIR
 #annotate SVtab
-bash Annotate_SVtab.sh  PATH_TO_OUTPUT_DIR/OUT_DIR
+bash Annotate_SVtab.sh  PATH_TO_OUTPUT_DIR
 #update postion with
-bash update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt  PATH_TO_OUTPUT_DIR/OUT_DIR
+bash update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt  PATH_TO_OUTPUT_DIR
 #simulate allelic ratios and coverage
 bash simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
 
