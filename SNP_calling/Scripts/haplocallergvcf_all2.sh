8de6b16adb91ec1503747e7606c8247b6079c670
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:10:45 +0100

    Update haplocallergvcf_all2.sh

--- SNP_calling/Scripts/haplocallergvcf_all2.sh
+++ SNP_calling/Scripts/haplocallergvcf_all2.sh
@@ -7,14 +7,14 @@ samples=$2
 bootrun=$3
 maxjob=$(expr $(expr $4 - 1 ) / 8 )
 
-gatk2='/netscratch/dep_coupland/grp_coupland/bioinformatics/bastiaan/software/gatk-4.2.0.0/gatk'
+gatk2='PATH_TO_GATK/gatk-4.2.0.0/gatk'
 
 haplocaller () {
 	local j="$1"
 	samtools index $j &&
 	Out=$(echo $j | rev | cut -d '_' -f2- | rev)
 	$gatk2 --java-options "-XX:ConcGCThreads=2 -XX:ActiveProcessorCount=8 -Xmx10g" HaplotypeCaller \
-   		-R /netscratch/dep_coupland/grp_fulgione/bastiaan/data/reference/Alpina_V5.1/Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
+   		-R Arabis_alpina.MPIPZ.version_5.1.chr.all.fasta \
     		-I $j \
     		-O ${Out}_b${bootrun}_allsites.g.vcf \
     		-ERC GVCF \
