085c2b4c58ca26189399b16f0a3055c3b22ed7b9
btjeng <73910014+btjeng@users.noreply.github.com>
2024-03-26 14:31:42 +0100

    Update Pipeline_sim.sh

--- Simulations/Pipeline_sim.sh
+++ Simulations/Pipeline_sim.sh
@@ -3,24 +3,23 @@
 
 ###transform all files 
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SC.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
+bash transform_mutation_SC.sh PATH_TO_OUTPUT_DIR/OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/transform_mutation_SV.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
+bash transform_mutation_SV.sh  PATH_TO_OUTPUT_DIR/OUT_DIR
 
 ####pipeline for single run
 ##convert to vcf
 #SC
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SC.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
+bash run_genotypetype_to_mat_SC.sh REP_NUMBER  PATH_TO_OUTPUT_DIR/OUT_DIR
 #SV
-bash ~/scripts/ParaMask_simulation_scripts/run_genotypetype_to_mat_SV_new.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
-#concat SV duplicates
-bash ~/scripts/ParaMask_simulation_scripts/concat_SV_mat_new.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
+bash run_genotypetype_to_mat_SV_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIRconcat SV duplicates
+bash concat_SV_mat_new.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIR
 #concat SV with SC
-bash ~/scripts/ParaMask_simulation_scripts/concat_SC_SV_mat.sh 1 /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final2
+bash concat_SC_SV_mat.sh REP_NUMBER PATH_TO_OUTPUT_DIR/OUT_DIR
 #annotate SVtab
-bash ~/scripts/ParaMask_simulation_scripts/Annotate_SVtab.sh /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final/SVtab_0.5_rep1.txt
+bash Annotate_SVtab.sh  PATH_TO_OUTPUT_DIR/OUT_DIR
 #update postion with
-bash ~/scripts/ParaMask_simulation_scripts/update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt /netscratch/dep_coupland/grp_fulgione/bastiaan/data/SeDuS5_0.5_reps_final
+bash update_pos_sim_mat.sh SVtab_0.5_rep1_annotated.txt Simulations_SV_SC_rep1_mat.txt  PATH_TO_OUTPUT_DIR/OUT_DIR
 #simulate allelic ratios and coverage
-bash ~/scripts/ParaMask_simulation_scripts/simulate_coverage_allele_depth.sh Simulations_SV_SC_rep1_mat_cpos.txt
+bash simulate_coverage_allele_depth.sh Simulations_SV_SC_rep{REP_NUMBER}_mat_cpos.txt
 
